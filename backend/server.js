90df3d508bfb6c3de2b138a68df5b82ba54e410d
Rafi <ruff6996@gmail.com>
2021-12-11 19:09:17 +0200

    paypal button

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ mongoose
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
-
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
